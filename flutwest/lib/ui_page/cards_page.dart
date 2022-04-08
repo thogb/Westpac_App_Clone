fdc3240919c198e28d47817cfd65ff37ef60d806
Tao Hu <taohu22@qq.com>
2022-04-08 14:15:48 +0800

    updated old scaffold pages accesible from nav bar to using custom list view with sliver appbar

--- flutwest/lib/ui_page/cards_page.dart
+++ flutwest/lib/ui_page/cards_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutwest/cust_widget/cust_button.dart';
 import 'package:flutwest/cust_widget/cust_heading.dart';
+import 'package:flutwest/cust_widget/cust_silver_appbar.dart';
 import 'package:flutwest/cust_widget/cust_text_button.dart';
 import 'package:flutwest/cust_widget/standard_padding.dart';
 
@@ -14,54 +15,58 @@ class CardsPage extends StatefulWidget {
 class _CardsPageState extends State<CardsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Cards")),
-      body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text(
-            "Westpac Debit\nMastercard\u00AE",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-          ),
+    return CustomScrollView(
+      slivers: [
+        const CustSilverAppbar(title: "Cards"),
+        SliverList(
+            delegate: SliverChildListDelegate([
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              //Security
-              CustHeading(heading: "Security"),
-              //lock cards
-              CustTextButton(heading: "Report lost or stolen"),
-              CustTextButton(heading: "Set or change PIN"),
-              CustTextButton(heading: "Reissue Card"),
-              CustTextButton(heading: "Notify of overseas travel"),
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Westpac Debit\nMastercard\u00AE",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  //Security
+                  CustHeading(heading: "Security"),
+                  //lock cards
+                  CustTextButton(heading: "Report lost or stolen"),
+                  CustTextButton(heading: "Set or change PIN"),
+                  CustTextButton(heading: "Reissue Card"),
+                  CustTextButton(heading: "Notify of overseas travel"),
 
-              //Wallets
-              CustHeading(heading: "Wallets"),
-              CustTextButton(heading: "Other wallets and Wearables"),
+                  //Wallets
+                  CustHeading(heading: "Wallets"),
+                  CustTextButton(heading: "Other wallets and Wearables"),
 
-              //Controls
-              CustHeading(heading: "Controls"),
-              CustTextButton(heading: "Gamlbing Block"),
-              CustTextButton(heading: "Activate card"),
+                  //Controls
+                  CustHeading(heading: "Controls"),
+                  CustTextButton(heading: "Gamlbing Block"),
+                  CustTextButton(heading: "Activate card"),
 
-              //Linked Account
-              CustHeading(heading: "Linked accounts"),
-              StandardPadding(
-                  child: CustButton(
-                      leftWidget: Text(
-                        "Westpac Choice",
-                        style:
-                            TextStyle(fontSize: CustButton.buttonHeadingSize),
-                      ),
-                      heading: "\n",
-                      rightWidget: Text("\$1333.33",
-                          style: CustButton.buttonHeadingStyle))),
-              SizedBox(height: 30.0)
+                  //Linked Account
+                  CustHeading(heading: "Linked accounts"),
+                  StandardPadding(
+                      child: CustButton(
+                          leftWidget: Text(
+                            "Westpac Choice",
+                            style: TextStyle(
+                                fontSize: CustButton.buttonHeadingSize),
+                          ),
+                          heading: "\n",
+                          rightWidget: Text("\$1333.33",
+                              style: CustButton.buttonHeadingStyle))),
+                  SizedBox(height: 30.0)
+                ],
+              ),
             ],
-          ),
-        ],
-      )),
+          )
+        ]))
+      ],
     );
   }
 }
--- flutwest/lib/ui_page/products_page.dart
+++ flutwest/lib/ui_page/products_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutwest/cust_widget/cust_silver_appbar.dart';
 import 'package:flutwest/model/vars.dart';
 
 import '../cust_widget/cust_text_button.dart';
@@ -8,46 +9,40 @@ class ProductsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
-          bottomOpacity: 0.0,
-          titleTextStyle: Vars.appbarTitleStyle,
-          title: const Text("Products"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: const [
-              CustTextButton(
-                heading: "Bank accounts",
-              ),
-              CustTextButton(
-                heading: "Home loans",
-              ),
-              CustTextButton(
-                heading: "Credit cards",
-              ),
-              CustTextButton(
-                heading: "Personal loans",
-              ),
-              CustTextButton(
-                heading: "International & travel",
-              ),
-              CustTextButton(
-                heading: "Insurance",
-              ),
-              CustTextButton(
-                heading: "Shares & investing",
-              ),
-              CustTextButton(
-                heading: "Superannuation",
-              ),
-              CustTextButton(
-                heading: "Business products",
-              )
-            ],
+    return CustomScrollView(
+      slivers: [
+        const CustSilverAppbar(title: "Products"),
+        SliverList(
+            delegate: SliverChildListDelegate(const [
+          CustTextButton(
+            heading: "Bank accounts",
           ),
-        ));
+          CustTextButton(
+            heading: "Home loans",
+          ),
+          CustTextButton(
+            heading: "Credit cards",
+          ),
+          CustTextButton(
+            heading: "Personal loans",
+          ),
+          CustTextButton(
+            heading: "International & travel",
+          ),
+          CustTextButton(
+            heading: "Insurance",
+          ),
+          CustTextButton(
+            heading: "Shares & investing",
+          ),
+          CustTextButton(
+            heading: "Superannuation",
+          ),
+          CustTextButton(
+            heading: "Business products",
+          )
+        ]))
+      ],
+    );
   }
 }
