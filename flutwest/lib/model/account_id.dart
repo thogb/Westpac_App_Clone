bd192334f27907caae1c0a96a06c327016fbdc8a
Tao Hu <taohu22@qq.com>
2022-05-01 19:09:11 +0800

    added AccountIDOrder, for storing ordering of accounts with sqlite

--- flutwest/lib/model/account_id.dart
+++ flutwest/lib/model/account_id.dart
@@ -12,3 +12,19 @@ class AccountID {
 
   set setBsb(bsb) => this.bsb = bsb;
 }
+
+class AccountIDOrder {
+  late AccountID accountID;
+  late int order;
+
+  AccountIDOrder({required number, required bsb, required this.order})
+      : accountID = AccountID(number: number, bsb: bsb);
+
+  get getAccountID => this.accountID;
+
+  set setAccountID(accountID) => this.accountID = accountID;
+
+  get getOrder => this.order;
+
+  set setOrder(order) => this.order = order;
+}
