bb9d15b5fc1b1eceb462020a54c09f1f747e6593
Tao Hu <taohu22@qq.com>
2022-05-11 19:24:49 +0800

    minor changes

--- flutwest/lib/ui_page/transaction_detail_page.dart
+++ flutwest/lib/ui_page/transaction_detail_page.dart
@@ -1,5 +1,3 @@
-import 'dart:collection';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutwest/controller/firestore_controller.dart';
