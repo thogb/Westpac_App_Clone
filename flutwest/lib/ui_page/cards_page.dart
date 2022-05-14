61bf91789f93f4dc32986c4d3a5b92b0d5930a6c
Tao Hu <taohu22@qq.com>
2022-05-14 16:08:56 +0800

    Finished Page and funcationality with firestore

--- flutwest/lib/ui_page/cards_page.dart
+++ flutwest/lib/ui_page/cards_page.dart
@@ -1,13 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutwest/controller/firestore_controller.dart';
 import 'package:flutwest/cust_widget/cust_button.dart';
 import 'package:flutwest/cust_widget/cust_heading.dart';
 import 'package:flutwest/cust_widget/cust_silver_appbar.dart';
 import 'package:flutwest/cust_widget/cust_text_button.dart';
+import 'package:flutwest/cust_widget/loading_text.dart';
 import 'package:flutwest/cust_widget/standard_padding.dart';
+import 'package:flutwest/model/account.dart';
+import 'package:flutwest/model/bank_card.dart';
 import 'package:flutwest/model/vars.dart';
+import 'package:flutwest/ui_page/account_detail_page.dart';
+import 'package:flutwest/ui_page/loading_page.dart';
+import 'package:flutwest/ui_page/lock_card_info_page.dart';
 
 class CardsPage extends StatefulWidget {
-  const CardsPage({Key? key}) : super(key: key);
+  final String cardNumber;
+  final Account cardAccount;
+  final List<AccountOrderInfo> accountOrderInfos;
+
+  const CardsPage(
+      {Key? key,
+      required this.cardNumber,
+      required this.cardAccount,
+      required this.accountOrderInfos})
+      : super(key: key);
 
   @override
   _CardsPageState createState() => _CardsPageState();
@@ -22,7 +39,17 @@ class _CardsPageState extends State<CardsPage> {
   static const double cardInfoTrailingSize = 14.0;
   static const double cardInfoTitleGap = 4.0;
 
-  bool _lockCard = false;
+  bool? _lockCard;
+  bool _showDigitalCard = false;
+  late BankCard _bankCard;
+  late final Future<DocumentSnapshot<Map<String, dynamic>>> _futureCard;
+
+  @override
+  void initState() {
+    _futureCard = FirestoreController.instance.getBankCard(widget.cardNumber);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,44 +58,84 @@ class _CardsPageState extends State<CardsPage> {
         const CustSilverAppbar(title: "Cards"),
         SliverList(
             delegate: SliverChildListDelegate([
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _getCard(),
-              const Text(
-                "Westpac Debit\nMastercard\u00AE",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //card info
-                  const SizedBox(height: 10.0),
+          FutureBuilder(
+              future: _futureCard,
+              builder: ((context,
+                  AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>>
+                      snapshot) {
+                if (snapshot.hasError) {
+                  return const Align(
+                      alignment: Alignment.center,
+                      child: Text("Error loading card"));
+                }
 
-                  _getShowCardInfo(),
-                  _getLockButton(),
-                  _getBottomButtons()
-                ],
-              ),
-            ],
-          )
+                bool loading =
+                    snapshot.connectionState == ConnectionState.waiting;
+
+                if (snapshot.hasData) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      !snapshot.data!.exists) {
+                    return const Align(
+                        alignment: Alignment.center,
+                        child: Text("Card not found"));
+                  }
+
+                  _bankCard = BankCard.fromMap(snapshot.data!.data()!);
+                  _lockCard ??= _bankCard.locked;
+                }
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    loading
+                        ? _getCard(
+                            Vars.loadingDummyColor, Vars.loadingDummyColor)
+                        : _getCard(Colors.red[600], Colors.red[900]),
+                    Text(
+                      "Westpac Debit\nMastercard\u00AE",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: loading ? Colors.transparent : Colors.black),
+                    ),
+                    !loading
+                        ? Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              //card info
+                              const SizedBox(height: 10.0),
+
+                              _getShowCardInfo(),
+                              _getLockButton(),
+                              _getBottomButtons()
+                            ],
+                          )
+                        : Column(
+                            children: const [
+                              LoadingText(),
+                              LoadingText(),
+                              LoadingText()
+                            ],
+                          ),
+                  ],
+                );
+              }))
         ]))
       ],
     );
   }
 
-  Widget _getCard() {
+  Widget _getCard(Color? unlocked, Color? locked) {
     return Container(
         margin: const EdgeInsets.symmetric(vertical: 20.0),
         width: 280.0,
         height: 180.0,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15.0),
-            color: !_lockCard ? Colors.red[600] : Colors.red[900]),
-        child: !_lockCard
-            ? null
-            : Column(
+            color: _lockCard != null && !_lockCard! ? unlocked : locked),
+        child: _lockCard != null && _lockCard!
+            ? Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: const [
@@ -81,7 +148,8 @@ class _CardsPageState extends State<CardsPage> {
                       "Locked temporarily",
                       style: TextStyle(color: Colors.white, fontSize: 16.0),
                     )
-                  ]));
+                  ])
+            : null);
   }
 
   Widget _getCardInfoTile(String title, String subtitle, Widget? trailer) {
@@ -108,17 +176,24 @@ class _CardsPageState extends State<CardsPage> {
         highlightColor: Colors.transparent,
       ),
       child: ExpansionTile(
+        onExpansionChanged: ((value) {
+          setState(() {
+            _showDigitalCard = value;
+          });
+        }),
         childrenPadding: const EdgeInsets.symmetric(vertical: 0.0),
         title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
+          children: [
+            const Text(
               "Card number",
               style: cardInfoTitleStyle,
             ),
-            SizedBox(height: cardInfoTitleGap),
+            const SizedBox(height: cardInfoTitleGap),
             Text(
-              "* * * *  * * * *  * * * *  8888",
+              !_showDigitalCard
+                  ? "* * * *  * * * *  * * * *  ${_bankCard.fourthFourDigit}"
+                  : "${_bankCard.firstFourDigit} ${_bankCard.secondFourDigit} ${_bankCard.thirdFourDigit} ${_bankCard.fourthFourDigit}",
               style: cardInfoSubTitleStyle,
             )
           ],
@@ -127,8 +202,8 @@ class _CardsPageState extends State<CardsPage> {
             style: TextStyle(
                 color: cardInfoTrailingColor, fontSize: cardInfoTrailingSize)),
         children: [
-          _getCardInfoTile("Name", "TAO HU", null),
-          _getCardInfoTile("Expiry", "02/23", null),
+          _getCardInfoTile("Name", _bankCard.name, null),
+          _getCardInfoTile("Expiry", _bankCard.expiryString, null),
           _getCardInfoTile(
               "Dynamic CVC",
               "061",
@@ -185,22 +260,69 @@ class _CardsPageState extends State<CardsPage> {
     return SwitchListTile(
         activeColor: Colors.blue[900],
         title: const Text("Lock card Temporarily"),
-        subtitle: !_lockCard
+        subtitle: !_lockCard!
             ? const Text("Unlocked")
             : Row(
                 children: [
                   const Text("Temporarily locked."),
-                  Icon(
-                    Icons.info_outline,
-                    color: cardInfoTrailingColor,
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                              pageBuilder:
+                                  ((context, animation, secondaryAnimation) =>
+                                      const LockCardInfoPage()),
+                              transitionDuration: const Duration(seconds: 0)));
+                    },
+                    child: Icon(
+                      Icons.info_outline,
+                      color: cardInfoTrailingColor,
+                    ),
                   )
                 ],
               ),
-        value: _lockCard,
-        onChanged: (bool value) {
+        value: _lockCard!,
+        onChanged: (bool value) async {
           setState(() {
             _lockCard = value;
           });
+          await Navigator.push(
+              context,
+              PageRouteBuilder(
+                  pageBuilder: ((context, animation, secondaryAnimation) =>
+                      LoadingPage(
+                          futureObject: FirestoreController.instance
+                              .updateBankCardLockStatus(
+                                  widget.cardNumber, value))),
+                  transitionDuration: const Duration(seconds: 0),
+                  reverseTransitionDuration: const Duration(seconds: 0)));
+          if (value == false) {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text("Your card is unlocked"),
+                    content: const Text("You can use your card as normal"),
+                    actions: [
+                      TextButton(
+                          style: TextButton.styleFrom(
+                              padding: const EdgeInsets.all(0)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Ok")),
+                    ],
+                  );
+                });
+          } else {
+            Navigator.push(
+                context,
+                PageRouteBuilder(
+                    pageBuilder: ((context, animation, secondaryNimation) =>
+                        const LockCardInfoPage()),
+                    transitionDuration: const Duration(seconds: 0)));
+          }
         });
   }
 
@@ -257,13 +379,28 @@ class _CardsPageState extends State<CardsPage> {
         ),
         StandardPadding(
             child: CustButton(
-                onTap: () {},
-                leftWidget: const Text(
-                  "Westpac Choice",
-                  style: TextStyle(fontSize: Vars.headingTextSize2),
+                onTap: () {
+                  int index = 0;
+                  for (int i = 0; i < widget.accountOrderInfos.length; i++) {
+                    if (widget.cardAccount.getNumber ==
+                        widget.accountOrderInfos[i].getAccount().getNumber) {
+                      index = i;
+                      break;
+                    }
+                  }
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => AccountDetailPage(
+                              accounts: widget.accountOrderInfos,
+                              currIndex: index)));
+                },
+                leftWidget: Text(
+                  "Westpac ${widget.cardAccount.getType}",
+                  style: const TextStyle(fontSize: Vars.headingTextSize2),
                 ),
                 heading: "\n",
-                rightWidget: const Text("\$1333.33",
+                rightWidget: Text("\$${widget.cardAccount.getBalance}",
                     style: CustButton.buttonHeadingStyle))),
         const SizedBox(height: 30.0)
       ],
