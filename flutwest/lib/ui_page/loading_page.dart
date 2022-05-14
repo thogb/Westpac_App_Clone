42809ae23f6afc6a7c45a5d67ae630b0ec855b8c
Tao Hu <taohu22@qq.com>
2022-05-14 14:45:31 +0800

    Loading page to display waiting for some response

---
+++ flutwest/lib/ui_page/loading_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutwest/cust_widget/big_circular_loading.dart';
+
+class LoadingPage extends StatelessWidget {
+  final Future<dynamic> futureObject;
+  const LoadingPage({Key? key, required this.futureObject}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    awaitFutureObject(context);
+    return const Scaffold(
+      body: Center(
+        child: BigCircularLoading(),
+      ),
+    );
+  }
+
+  void awaitFutureObject(BuildContext context) async {
+    await futureObject;
+    Navigator.pop(context);
+  }
+}
