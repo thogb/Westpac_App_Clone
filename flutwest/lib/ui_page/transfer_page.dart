83ca30ae0a9f6d3cc82bfe15c2a3d7d662d60acf
Tao Hu <taohu22@qq.com>
2022-05-20 12:17:55 +0800

    test

--- flutwest/lib/ui_page/transfer_page.dart
+++ flutwest/lib/ui_page/transfer_page.dart
@@ -5,7 +5,10 @@ import 'package:flutwest/cust_widget/cust_text_field.dart';
 import 'package:flutwest/cust_widget/in_text_button.dart';
 import 'package:flutwest/cust_widget/standard_padding.dart';
 import 'package:flutwest/model/account.dart';
+import 'package:flutwest/model/utils.dart';
 import 'package:flutwest/model/vars.dart';
+import 'package:flutwest/ui_page/schedule_pay_page.dart';
+import 'package:flutwest/ui_page/transfer_from_page.dart';
 
 class TransferPage extends StatefulWidget {
   final List<Account> accounts;
@@ -20,8 +23,12 @@ class TransferPage extends StatefulWidget {
 
 class _TransferPageState extends State<TransferPage> {
   bool _enableTransferButton = false;
+  DateTime _dateTime = DateTime.now();
+
   late Account _currAccount;
   late Account _toAccount;
+
+  final TextEditingController _tecMoney = TextEditingController();
 
   @override
   void initState() {
@@ -60,9 +67,32 @@ class _TransferPageState extends State<TransferPage> {
                 // From this account
                 const SizedBox(height: Vars.heightGapBetweenWidgets * 0.5),
                 InTextButton.standard(
-                    leftLabel: "From ",
-                    label: _currAccount.getAccountName,
-                    rightLabel: ""),
+                  leftLabel: "From ",
+                  label: _currAccount.getAccountName,
+                  rightLabel: "",
+                  onTap: () async {
+                    List<Account> inAccounts = List.from(widget.accounts);
+
+                    inAccounts.remove(_currAccount);
+                    Object? result = await Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                            transitionDuration: Duration.zero,
+                            reverseTransitionDuration: Duration.zero,
+                            pageBuilder: (_, __, ___) => TransferFromPage(
+                                accounts: inAccounts, requestResult: true)));
+                    if (result != null) {
+                      Account resultAccount = result as Account;
+
+                      setState(() {
+                        if (resultAccount.getNumber == _toAccount.getNumber) {
+                          _toAccount = _currAccount;
+                        }
+                        _currAccount = resultAccount;
+                      });
+                    }
+                  },
+                ),
                 const SizedBox(height: Vars.gapBetweenTextVertical),
                 Text(_currAccount.getBalanceUSDToString),
                 const SizedBox(height: Vars.heightGapBetweenWidgets),
@@ -72,7 +102,33 @@ class _TransferPageState extends State<TransferPage> {
                     ? InTextButton.standard(
                         leftLabel: "To ",
                         label: _toAccount.getAccountName,
-                        rightLabel: "")
+                        rightLabel: "",
+                        onTap: () async {
+                          List<Account> inAccounts = List.from(widget.accounts);
+
+                          inAccounts.remove(_toAccount);
+                          Object? result = await Navigator.push(
+                              context,
+                              PageRouteBuilder(
+                                  transitionDuration: Duration.zero,
+                                  reverseTransitionDuration: Duration.zero,
+                                  pageBuilder: (_, __, ___) => TransferFromPage(
+                                      title: "To",
+                                      accounts: inAccounts,
+                                      requestResult: true)));
+                          if (result != null) {
+                            Account resultAccount = result as Account;
+
+                            setState(() {
+                              if (resultAccount.getNumber ==
+                                  _currAccount.getNumber) {
+                                _currAccount = _toAccount;
+                              }
+                              _toAccount = resultAccount;
+                            });
+                          }
+                        },
+                      )
                     : InTextButton.noButton(
                         leftLabel: "To ",
                         rightLabel: _toAccount.getAccountName),
@@ -85,10 +141,33 @@ class _TransferPageState extends State<TransferPage> {
                 // Input amount text field
                 const Text("Amount", style: Vars.headingStyle3),
                 CustTextField.moneyInput(
+                  controller: _tecMoney,
+                  onChanged: (String value) {
+                    Decimal? amount =
+                        Decimal.tryParse(value.replaceAll(r",", ""));
+
+                    if (amount == null) {
+                      if (_enableTransferButton) {
+                        setState(() {
+                          _enableTransferButton = false;
+                        });
+                      }
+                    } else {
+                      if (!_enableTransferButton) {
+                        setState(() {
+                          _enableTransferButton = true;
+                        });
+                      }
+                    }
+                  },
                   getErrorMsg: (String value) {
-                    if (Decimal.parse(value.replaceAll(r",", "")) >
-                        _currAccount.balance) {
-                      return "Insufficient funds in your ${_currAccount.getAccountName} account.";
+                    Decimal? inputAmount =
+                        Decimal.tryParse(value.replaceAll(r",", ""));
+
+                    if (inputAmount != null) {
+                      if (inputAmount > _currAccount.balance) {
+                        return "Insufficient funds in your ${_currAccount.getAccountName} account.";
+                      }
                     }
 
                     return null;
@@ -98,7 +177,28 @@ class _TransferPageState extends State<TransferPage> {
 
                 // Schedule Date
                 InTextButton.standard(
-                    leftLabel: "Schedule for ", label: "Today", rightLabel: ""),
+                    leftLabel: "Schedule for ",
+                    label: Utils.getDateTimeWDDMYToday(_dateTime),
+                    rightLabel: "",
+                    onTap: () async {
+                      Object? result = await Navigator.push(
+                          context,
+                          PageRouteBuilder(
+                              pageBuilder:
+                                  ((context, animation, secondaryAnimation) =>
+                                      SchedulePayPage(dateTime: _dateTime)),
+                              transitionDuration: Duration.zero,
+                              reverseTransitionDuration: Duration.zero));
+                      if (result != null) {
+                        DateTime newDateTime = result as DateTime;
+
+                        if (!Vars.isSameDay(newDateTime, _dateTime)) {
+                          setState(() {
+                            _dateTime = newDateTime;
+                          });
+                        }
+                      }
+                    }),
                 const SizedBox(height: Vars.heightGapBetweenWidgets * 1.5),
 
                 // Desription
@@ -127,7 +227,45 @@ class _TransferPageState extends State<TransferPage> {
               backgroundColor: _enableTransferButton
                   ? Vars.clickAbleColor
                   : Vars.disabledClickableColor),
-          onPressed: () {},
+          onPressed: !_enableTransferButton
+              ? null
+              : () {
+                  showDialog(
+                      context: context,
+                      builder: (contex) {
+                        Decimal? amount = Decimal.tryParse(
+                            _tecMoney.text.replaceAll(r",", ""));
+                        if (amount != null) {
+                          return AlertDialog(
+                            title: const Text("Check details"),
+                            content: Text(
+                                "\$${Utils.formatDecimalMoneyUS(amount)}\n${Utils.getDateTimeWDDMYToday(_dateTime)} (Perth Time)"),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: const Text("Cancel")),
+                              TextButton(
+                                  onPressed: () {},
+                                  child: const Text("Transfer"))
+                            ],
+                          );
+                        } else {
+                          return AlertDialog(
+                              title: const Text("Error"),
+                              content: const Text(
+                                  "Failed to read input please try again."),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text("Ok"))
+                              ]);
+                        }
+                      });
+                },
           child: const Center(
               heightFactor: 0,
               child: Text(
