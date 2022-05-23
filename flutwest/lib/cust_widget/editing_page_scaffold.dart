0d43ab9213fc9fcd407872f6ecd2119bfa195a75
Tao Hu <taohu22@qq.com>
2022-05-23 21:17:15 +0800

    This type of scaffold page style happened to be for all pages that does some sort of data editing and saving, made intoo stateful widget

---
+++ flutwest/lib/cust_widget/editing_page_scaffold.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutwest/model/vars.dart';
+
+class EditingPageScaffold extends StatefulWidget {
+  static const leadingIconDefault =
+      Icon(Icons.close, color: Vars.clickAbleColor);
+  static const leadingIconArrowBack =
+      Icon(Icons.arrow_back, color: Vars.clickAbleColor);
+
+  final String? title;
+  final Icon leadingIcon;
+  final VoidCallback? leadingOnTap;
+  final List<Widget> content;
+  final Widget? floatingActionButton;
+
+  const EditingPageScaffold(
+      {Key? key,
+      this.title,
+      required this.content,
+      this.floatingActionButton,
+      this.leadingIcon = leadingIconDefault,
+      this.leadingOnTap})
+      : super(key: key);
+
+  @override
+  _EditingPageScaffoldState createState() => _EditingPageScaffoldState();
+}
+
+class _EditingPageScaffoldState extends State<EditingPageScaffold> {
+  final ScrollController _scrollController = ScrollController();
+
+  Widget? title;
+
+  @override
+  void initState() {
+    _scrollController.addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_onScroll);
+    _scrollController.dispose();
+
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (widget.title != null) {
+      if (_scrollController.offset > 20) {
+        if (title == null) {
+          setState(() {
+            title = Text(widget.title!);
+          });
+        }
+      } else {
+        if (title != null) {
+          setState(() {
+            title = null;
+          });
+        }
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: GestureDetector(
+          child: widget.leadingIcon,
+          onTap: widget.leadingOnTap ??
+              () {
+                Navigator.pop(context);
+              },
+        ),
+        title: title,
+      ),
+      body: SingleChildScrollView(
+        controller: _scrollController,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: Vars.standardPaddingSize * 2),
+          child: Column(
+            children: widget.content,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: widget.floatingActionButton,
+    );
+  }
+}
