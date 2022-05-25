30d1fdd48ad12961e1823fcca8b3c8a98cd57080
Tao Hu <taohu22@qq.com>
2022-05-25 21:22:51 +0800

    Made more factories for cust heading

--- flutwest/lib/cust_widget/cust_heading.dart
+++ flutwest/lib/cust_widget/cust_heading.dart
@@ -3,22 +3,55 @@ import 'package:flutwest/model/vars.dart';
 
 class CustHeading extends StatelessWidget {
   final String heading;
+  final TextStyle? textStyle;
+  final EdgeInsetsGeometry padding;
 
   static const TextStyle headingStyle = Vars.headingStyle1;
 
-  const CustHeading({Key? key, required this.heading}) : super(key: key);
+  const CustHeading(
+      {Key? key,
+      required this.heading,
+      required this.textStyle,
+      required this.padding})
+      : super(key: key);
+
+  factory CustHeading.big(
+      {required String heading,
+      TextStyle textStyle = const TextStyle(
+          fontSize: Vars.headingTextSize1, fontWeight: FontWeight.w600),
+      EdgeInsetsGeometry padding =
+          const EdgeInsets.symmetric(vertical: Vars.topBotPaddingSize)}) {
+    return CustHeading(
+        heading: heading, textStyle: textStyle, padding: padding);
+  }
+
+  factory CustHeading.medium(
+      {required String heading,
+      TextStyle textStyle = const TextStyle(
+          fontSize: Vars.headingTextSize2, fontWeight: FontWeight.w600),
+      EdgeInsetsGeometry padding =
+          const EdgeInsets.symmetric(vertical: Vars.topBotPaddingSize)}) {
+    return CustHeading(
+        heading: heading, textStyle: textStyle, padding: padding);
+  }
+
+  factory CustHeading.small(
+      {required String heading,
+      TextStyle textStyle = const TextStyle(
+          fontSize: Vars.headingTextSize3, fontWeight: FontWeight.w600),
+      EdgeInsetsGeometry padding =
+          const EdgeInsets.symmetric(vertical: Vars.topBotPaddingSize)}) {
+    return CustHeading(
+        heading: heading, textStyle: textStyle, padding: padding);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(
-          Vars.standardPaddingSize,
-          Vars.topBotPaddingSize + 10.0,
-          Vars.standardPaddingSize,
-          Vars.topBotPaddingSize),
+      padding: padding,
       child: Text(
         heading,
-        style: headingStyle,
+        style: textStyle,
       ),
     );
   }
