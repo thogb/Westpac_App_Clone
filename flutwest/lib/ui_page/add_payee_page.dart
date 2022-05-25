4d19a72da6d66938f0ab1f1decb9b7ebb34cdbea
Tao Hu <taohu22@qq.com>
2022-05-25 21:19:53 +0800

    Finished add payee page

--- flutwest/lib/ui_page/add_payee_page.dart
+++ flutwest/lib/ui_page/add_payee_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutwest/controller/firestore_controller.dart';
+import 'package:flutwest/controller/sqlite_controller.dart';
 import 'package:flutwest/cust_widget/cust_floating_button.dart';
 import 'package:flutwest/cust_widget/cust_text_field.dart';
 import 'package:flutwest/cust_widget/editing_page_scaffold.dart';
 import 'package:flutwest/cust_widget/in_text_button.dart';
+import 'package:flutwest/model/account.dart';
+import 'package:flutwest/model/payee.dart';
+import 'package:flutwest/model/utils.dart';
 import 'package:flutwest/model/vars.dart';
+import 'package:flutwest/ui_page/loading_page.dart';
+import 'package:flutwest/ui_page/payment_page.dart';
 
 class AddPayeePage extends StatefulWidget {
-  const AddPayeePage({Key? key}) : super(key: key);
+  final List<Account> accounts;
+  final Account currAccount;
+  final String memberId;
+  const AddPayeePage(
+      {Key? key,
+      required this.memberId,
+      required this.accounts,
+      required this.currAccount})
+      : super(key: key);
 
   @override
   _AddPayeePageState createState() => _AddPayeePageState();
@@ -35,7 +50,14 @@ class _AddPayeePageState extends State<AddPayeePage> {
           CustTextField.standardSmall(
             label: "Account name",
             controller: _tecAccountName,
-            onChanged: onTextFieldChanged,
+            onChanged: _onTextFieldChanged,
+            onFocusChange: (bool isFocus) {
+              if (!isFocus &&
+                  _tecAccountName.text.trim().isNotEmpty &&
+                  _tecNickName.text.trim().isEmpty) {
+                _tecNickName.text = _tecAccountName.text.trim();
+              }
+            },
           ),
           const SizedBox(height: Vars.heightGapBetweenWidgets),
 
@@ -45,7 +67,7 @@ class _AddPayeePageState extends State<AddPayeePage> {
               label: "BSB",
               controller: _tecBSB,
               keyboardType: TextInputType.number,
-              onChanged: onTextFieldChanged),
+              onChanged: _onTextFieldChanged),
           const SizedBox(height: Vars.heightGapBetweenWidgets),
 
           // Input Account Number
@@ -54,7 +76,7 @@ class _AddPayeePageState extends State<AddPayeePage> {
               label: "Account number",
               controller: _tecAccountNumber,
               keyboardType: TextInputType.number,
-              onChanged: onTextFieldChanged),
+              onChanged: _onTextFieldChanged),
           const SizedBox(
             height: Vars.heightGapBetweenWidgets,
           ),
@@ -63,7 +85,7 @@ class _AddPayeePageState extends State<AddPayeePage> {
           CustTextField.standardSmall(
               label: "Nickname",
               controller: _tecNickName,
-              onChanged: onTextFieldChanged),
+              onChanged: _onTextFieldChanged),
           const SizedBox(height: Vars.heightGapBetweenWidgets),
 
           // To not get blocked by floating text button
@@ -71,13 +93,140 @@ class _AddPayeePageState extends State<AddPayeePage> {
         ],
         floatingActionButton: !_enableFloatingButton
             ? CustFloatingButton.disabled(title: "Next")
-            : CustFloatingButton.enabled(title: "Next", onPressed: () {}));
+            : CustFloatingButton.enabled(
+                title: "Next",
+                onPressed: () async {
+                  if (_tecAccountName.text.trim().length < 2) {
+                    _showAlertDialog("Invalid Input",
+                        "Account name must consist of at least 2 character");
+                    return;
+                  }
+
+                  if (_tecNickName.text.trim().length < 2) {
+                    _showAlertDialog("Invalid Input",
+                        "Nick name must conssit of at least 2 character");
+                    return;
+                  }
+
+                  if (_tecBSB.text.length != 6) {
+                    _showAlertDialog(
+                        "Invalid Input", "BSB must consist of 6 digits");
+                    return;
+                  }
+
+                  if (_tecAccountNumber.text.length < 6 ||
+                      _tecAccountName.text.length > 9) {
+                    _showAlertDialog("Invalid Input",
+                        "Account number must consist of 6 to 9 digits");
+                    return;
+                  }
+
+                  Payee payee = Payee.noId(
+                      accountNumber: _tecAccountNumber.text,
+                      accountBSB:
+                          "${_tecBSB.text.substring(0, 3)}-${_tecBSB.text.substring(3, 6)}",
+                      accountName:
+                          Utils.getCapitalizedString(_tecAccountName.text),
+                      nickName: Utils.getCapitalizedString(_tecNickName.text));
+
+                  Object? result = await Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: ((context, animation,
+                                  secondaryAnimation) =>
+                              LoadingPage(
+                                  futureObject: SQLiteController.instance
+                                      .doesPayeeExist(widget.memberId, payee))),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero));
+
+                  bool doesPayeeExist = result as bool;
+
+                  if (doesPayeeExist) {
+                    _showAlertDialog("Payee already exist",
+                        "Payee with the inputted information already exist in the payee list");
+                    return;
+                  }
+
+                  DateTime addTime = DateTime.now();
+
+                  Object? result2 = await Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: ((context, animation,
+                                  secondaryAnimation) =>
+                              LoadingPage(
+                                  futureObject: FirestoreController.instance
+                                      .addPayee(
+                                          widget.memberId, payee, addTime))),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero));
+
+                  String docId = result2 as String;
+
+                  payee = Payee(
+                      docId: docId,
+                      accountNumber: _tecAccountNumber.text,
+                      accountBSB:
+                          "${_tecBSB.text.substring(0, 3)}-${_tecBSB.text.substring(3, 6)}",
+                      accountName:
+                          Utils.getCapitalizedString(_tecAccountName.text),
+                      nickName: Utils.getCapitalizedString(_tecNickName.text));
+
+                  await Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: ((context, animation,
+                                  secondaryAnimation) =>
+                              LoadingPage(
+                                  futureObject: SQLiteController.instance
+                                      .addPayee(
+                                          widget.memberId, payee, addTime))),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero));
+                  Navigator.pushReplacement(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder:
+                              ((context, animation, secondaryAnimation) =>
+                                  PaymentPage(
+                                      memberId: widget.memberId,
+                                      payee: payee,
+                                      accounts: widget.accounts,
+                                      currAccount: widget.currAccount)),
+                          transitionDuration: Duration.zero,
+                          reverseTransitionDuration: Duration.zero),
+                      result: payee);
+
+                  /*
+                  await FirestoreController.instance
+                      .addPayee(widget.memberId, payee, addTime);
+                  await SQLiteController.instance
+                      .addPayee(widget.memberId, payee, addTime);*/
+                }));
   }
 
-  void onTextFieldChanged(String value) {
+  void _showAlertDialog(String title, String content) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: Text(title),
+              content: Text(content),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Ok"))
+              ],
+            ));
+  }
+
+  void _onTextFieldChanged(String value) {
     if (_tecBSB.text.isNotEmpty &&
         _tecAccountName.text.isNotEmpty &&
-        _tecAccountNumber.text.isNotEmpty) {
+        _tecAccountNumber.text.isNotEmpty &&
+        _tecNickName.text.isNotEmpty) {
       if (!_enableFloatingButton) {
         setState(() {
           _enableFloatingButton = true;
