038d127931358497e2b005bcf0f1e712da3b1d70
Tao Hu <taohu22@qq.com>
2022-05-25 21:19:27 +0800

    Finished choose payee page

--- flutwest/lib/ui_page/choose_payee_page.dart
+++ flutwest/lib/ui_page/choose_payee_page.dart
@@ -13,19 +13,22 @@ import 'package:flutwest/cust_widget/editing_page_scaffold.dart';
 import 'package:flutwest/cust_widget/loading_text.dart';
 import 'package:flutwest/cust_widget/standard_padding.dart';
 import 'package:flutwest/model/account.dart';
+import 'package:flutwest/model/member.dart';
 import 'package:flutwest/model/payee.dart';
 import 'package:flutwest/model/utils.dart';
 import 'package:flutwest/model/vars.dart';
 import 'package:flutwest/ui_page/add_payee_page.dart';
+import 'package:flutwest/ui_page/payee_info_page.dart';
+import 'package:flutwest/ui_page/payment_page.dart';
 
 class ChoosePayeePage extends StatefulWidget {
-  final String meberId;
+  final String memberId;
   final DateTime? recentPayeeEdit;
   final List<Account> accounts;
   const ChoosePayeePage(
       {Key? key,
       required this.accounts,
-      required this.meberId,
+      required this.memberId,
       required this.recentPayeeEdit})
       : super(key: key);
 
@@ -35,6 +38,7 @@ class ChoosePayeePage extends StatefulWidget {
 
 class _ChoosePayeePageState extends State<ChoosePayeePage>
     with TickerProviderStateMixin {
+  static const String recentPaymentKey = "recent_payment";
   late final AnimationController _fakeAppBarController = AnimationController(
       duration: const Duration(milliseconds: 300), vsync: this);
 
@@ -60,13 +64,17 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
 
   String _currFilter = payeeFilters[1];
   double _elevationLevel = 0;
-  final HashSet<int> _alphabetHeaderIndexs = HashSet();
+  //final Map<String, bool> _alphabetHeaderIndexs = {};
   final List<Payee> _recentPayees = [];
+  final List<List<Payee>> _payeeGroups = [];
+  List<Payee> _payees = [];
+  bool _requireReconstruct = false;
+  String _filterKeyword = "";
 
   @override
   void initState() {
     _scrollController.addListener(_onScroll);
-    _futurePayees = _getPayees(widget.meberId, widget.recentPayeeEdit);
+    _futurePayees = _getPayees(widget.memberId, widget.recentPayeeEdit);
 
     super.initState();
   }
@@ -85,6 +93,12 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
         children: [_getFakeAppBar(), Expanded(child: _getPayeeList())],
       ),
     );
+  }
+
+  void _resetPayeeData() {
+    //_alphabetHeaderIndexs.clear();
+    _payeeGroups.clear();
+    _recentPayees.clear();
   }
 
   void _onScroll() {
@@ -147,12 +161,20 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
           // Search text field
           StandardPadding(
               child: CustTextFieldSearch(
+            hintText: "Search",
             textEditingController: _tecSearch,
-            onFocus: () {
-              _fakeAppBarController.forward();
+            onFocus: (bool isFocused) {
+              if (isFocused) {
+                _fakeAppBarController.forward();
+              }
+
+              _applyFilter();
             },
             onPrefixButtonTap: () {
               _fakeAppBarController.reverse();
+            },
+            onChanged: (String value) {
+              _applyFilter();
             },
           )),
           const SizedBox(height: Vars.heightGapBetweenWidgets),
@@ -192,6 +214,15 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
     );
   }
 
+  void _applyFilter() {
+    String processedValue = _tecSearch.text.toLowerCase().trim();
+    if (processedValue != _filterKeyword.toLowerCase()) {
+      setState(() {
+        _filterKeyword = processedValue;
+      });
+    }
+  }
+
   void _showBottomSheet() {
     TextStyle headingButtonStyle = const TextStyle(
         fontSize: Vars.headingTextSize3, fontWeight: FontWeight.w600);
@@ -208,14 +239,30 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
               CustTextButton(
                 headingTextStyle: headingButtonStyle,
                 heading: "BSB & Account",
-                onTap: () {
+                onTap: () async {
                   Navigator.pop(context);
-                  Navigator.push(
+                  Object? result = await Navigator.push(
                       context,
                       PageRouteBuilder(
                           pageBuilder:
                               ((context, animation, secondaryAnimation) =>
-                                  const AddPayeePage())));
+                                  AddPayeePage(
+                                    memberId: widget.memberId,
+                                    accounts: widget.accounts,
+                                    currAccount: widget.accounts[0],
+                                  ))));
+                  if (result != null) {
+                    Payee newPayee = result as Payee;
+
+                    _payees.add(newPayee);
+                    // _payees.sort(
+                    // ((a, b) => a.getNickName.compareTo(b.getNickName)));
+                    //_sortPayeeList(_payees);
+                    setState(() {
+                      _requireReconstruct = true;
+                      //_payees.length;
+                    });
+                  }
                 },
               ),
               CustTextButton(
@@ -265,10 +312,17 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
       if (recentPayeeEdit == null || recentPayeeEdit != memberRecentPayeeEdit) {
         var remotePayees =
             await FirestoreController.instance.getPayees(memberId);
-        SQLiteController.instance
-            .syncPayees(remotePayees: remotePayees, localPayees: localPayees);
+        SQLiteController.instance.syncPayees(
+            memberId: memberId,
+            remotePayees: remotePayees,
+            localPayees: localPayees,
+            recentPayeeDate: memberRecentPayeeEdit);
+        //remotePayees.sort(((a, b) => a.getNickName.compareTo(b.getNickName)));
+        _sortPayeeList(remotePayees);
         payees = remotePayees;
       } else {
+        //localPayees.sort(((a, b) => a.getNickName.compareTo(b.getNickName)));
+        _sortPayeeList(localPayees);
         payees = localPayees;
       }
     } else {
@@ -290,60 +344,148 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
           if (snapshot.hasData &&
               snapshot.connectionState == ConnectionState.done) {
             List<Payee>? payees = snapshot.data;
-            if (payees != null && _alphabetHeaderIndexs.isEmpty) {
+            if (payees != null) {
               if (payees.isEmpty) {
                 return const Align(
                     alignment: Alignment.topCenter, child: Text("No Payees"));
               }
 
-              String currChar;
-              DateTime now = DateTime.now();
-              DateTime sevenDayAgo = DateTime(now.year, now.month, now.day - 7);
-
-              for (int i = 0; i < 5; i++) {
-                if (i < payees.length &&
-                    payees[i].lastPayDate != null &&
-                    payees[i].lastPayDate!.isAfter(sevenDayAgo)) {
-                  _recentPayees.add(payees[i]);
+              if (_requireReconstruct || _payees.isEmpty) {
+                if (_payees.isEmpty) {
+                  _payees = payees;
                 }
-              }
+                _resetPayeeData();
+                DateTime now = DateTime.now();
+                DateTime sevenDayAgo =
+                    DateTime(now.year, now.month, now.day - 7);
 
-              currChar = payees[0].getNickName[0].toUpperCase();
-              _alphabetHeaderIndexs.add(0);
+                _payees.sort(((a, b) {
+                  if (a.lastPayDate == null) {
+                    return 1;
+                  }
 
-              for (int i = 0; i < payees.length; i++) {
-                if (currChar != payees[i].getNickName[i].toUpperCase()) {
-                  currChar = payees[i].getNickName[i].toUpperCase();
-                  _alphabetHeaderIndexs.add(i);
+                  if (b.lastPayDate == null) {
+                    return a.lastPayDate != null ? -1 : 1;
+                  }
+
+                  return a.lastPayDate!.compareTo(b.lastPayDate!) * -1;
+                }));
+
+                for (int i = 0; i < 5; i++) {
+                  if (i < _payees.length &&
+                      _payees[i].lastPayDate != null &&
+                      _payees[i].lastPayDate!.isAfter(sevenDayAgo)) {
+                    _recentPayees.add(_payees[i]);
+                  }
                 }
+
+                _sortPayeeList(payees);
+                //currChar = _payees[0].getNickName[0].toUpperCase();
+                String currChar = "";
+                List<Payee> group = [];
+
+                for (int i = 0; i < _payees.length; i++) {
+                  if (currChar == _payees[i].getNickName[0].toUpperCase()) {
+                    group.add(_payees[i]);
+                  } else {
+                    if (group.isNotEmpty) {
+                      _payeeGroups.add(group);
+                      //print(group.length);
+                    }
+                    group = [];
+                    group.add(_payees[i]);
+                    currChar = _payees[i].getNickName[0].toUpperCase();
+                  }
+                }
+                _payeeGroups.add(group);
+                _requireReconstruct = false;
               }
 
               return ListView.builder(
-                  itemCount: _recentPayees.length + payees.length,
+                  padding: EdgeInsets.zero,
+                  controller: _scrollController,
+                  itemCount:
+                      _payeeGroups.length + (_recentPayees.isEmpty ? 0 : 1),
                   itemBuilder: (context, index) {
-                    if (index < _recentPayees.length) {
-                      if (index == 0) {
-                        return Column(children: [
-                          _getPayeeHeader("Recently paid"),
-                          _getRecentPayeeButton(_recentPayees[index])
-                        ]);
-                      } else {
-                        return _getRecentPayeeButton(_recentPayees[index]);
+                    if (index == 0 && _recentPayees.isNotEmpty) {
+                      List<Payee> displayPayees = [];
+                      for (Payee payee in _recentPayees) {
+                        if (_payeeSatisfyFilter(payee)) {
+                          displayPayees.add(payee);
+                        }
                       }
-                    } else {
-                      int actualIndex = index - _recentPayees.length;
-                      Payee payee = payees[actualIndex];
 
-                      if (_alphabetHeaderIndexs.contains(actualIndex)) {
-                        return Column(
-                          children: [
-                            _getPayeeHeader(payee.getNickName[0].toUpperCase()),
-                            _getPayeeButton(payee)
-                          ],
-                        );
-                      } else {
-                        return _getPayeeButton(payee);
+                      if (displayPayees.isEmpty) {
+                        return const SizedBox();
                       }
+
+                      return Column(
+                          children: List.generate(
+                              displayPayees.length + 1,
+                              (index) => index == 0
+                                  ? _getPayeeHeader("Recently paid")
+                                  : _getRecentPayeeButton(
+                                      displayPayees[index - 1])));
+                      /*
+                      if (_payeeSatisfyFilter(_recentPayees[index])) {
+                        bool? displayHeader =
+                            _alphabetHeaderIndexs[recentPaymentKey];
+                        if (displayHeader != null && !displayHeader) {
+                          _alphabetHeaderIndexs[recentPaymentKey] = true;
+                          return Column(children: [
+                            _getPayeeHeader("Recently paid"),
+                            _getRecentPayeeButton(_recentPayees[index])
+                          ]);
+                        } else {
+                          return _getRecentPayeeButton(_recentPayees[index]);
+                        }
+                      }*/
+                    } else {
+                      int actualIndex = index - (_recentPayees.isEmpty ? 0 : 1);
+                      List<Payee> displayPayees = [];
+
+                      for (Payee payee in _payeeGroups[actualIndex]) {
+                        if (_payeeSatisfyFilter(payee)) {
+                          displayPayees.add(payee);
+                        }
+                      }
+
+                      if (displayPayees.isEmpty) {
+                        return const SizedBox();
+                      }
+
+                      return Column(
+                          children: List.generate(
+                              displayPayees.length + 1,
+                              (index) => index == 0
+                                  ? _getPayeeHeader(displayPayees
+                                      .first.getNickName[0]
+                                      .toUpperCase())
+                                  : _getPayeeButton(displayPayees[index - 1])));
+                      /*
+                      int actualIndex = index - _recentPayees.length;
+                      Payee payee = _payees[actualIndex];
+
+                      if (_payeeSatisfyFilter(payee)) {
+                        String initialUpper =
+                            payee.getNickName[0].toUpperCase();
+                        bool? displayHeader =
+                            _alphabetHeaderIndexs[initialUpper];
+                        if (displayHeader != null && !displayHeader) {
+                          _alphabetHeaderIndexs[initialUpper] = true;
+                          return Column(
+                            children: [
+                              _getPayeeHeader(
+                                  payee.getNickName[0].toUpperCase()),
+                              _getPayeeButton(payee)
+                            ],
+                          );
+                        } else {
+                          return _getPayeeButton(payee);
+                        }
+                      }
+                    }
+                    return const SizedBox();*/
                     }
                   });
             }
@@ -353,14 +495,20 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
         }));
   }
 
+  void _sortPayeeList(List<Payee> payeeList) {
+    payeeList.sort(((a, b) =>
+        a.getNickName.toUpperCase().compareTo(b.getNickName.toUpperCase())));
+  }
+
   Widget _getPayeeHeader(String title) {
     return Padding(
-      padding:
-          const EdgeInsets.symmetric(vertical: Vars.gapBetweenTextVertical),
+      padding: const EdgeInsets.symmetric(
+          vertical: Vars.gapBetweenTextVertical,
+          horizontal: Vars.standardPaddingSize),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(title, style: Vars.buttonPragraphStyle),
+          Text(title, style: const TextStyle(fontSize: Vars.headingTextSize3)),
           const SizedBox(height: Vars.gapBetweenTextVertical),
           const Divider(height: 0.5, color: Colors.black54)
         ],
@@ -370,7 +518,9 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
 
   Widget _getRecentPayeeButton(Payee payee) {
     return CustButton(
+      borderOn: false,
       heading: payee.getNickName,
+      headingStyle: const TextStyle(fontSize: Vars.headingTextSize2),
       paragraph: "${payee.getAccountID.getBsb} ${payee.getAccountID.getNumber}",
       rightWidget: Text(
         Utils.getDateTimeWDDNM(payee.lastPayDate!),
@@ -378,14 +528,74 @@ class _ChoosePayeePageState extends State<ChoosePayeePage>
           fontSize: Vars.headingTextSize3,
         ),
       ),
+      onTap: () async {
+        // This payee already on recent payee area no need to reconstruct
+        await handlePaymentPage(payee);
+      },
     );
   }
 
   Widget _getPayeeButton(Payee payee) {
     return CustButton(
+      borderOn: false,
       heading: payee.getNickName,
+      headingStyle: const TextStyle(fontSize: Vars.headingTextSize2),
       paragraph: "${payee.getAccountID.getBsb} ${payee.getAccountID.getNumber}",
-      rightWidget: const Icon(Icons.info),
+      rightWidget: GestureDetector(
+          child: const Icon(Icons.info_outline),
+          onTap: () async {
+            Object? result = await Navigator.push(
+                context,
+                PageRouteBuilder(
+                    pageBuilder: ((context, animation, secondaryAnimation) =>
+                        PayeeInfoPage(
+                            memberId: widget.memberId,
+                            payee: payee,
+                            accounts: widget.accounts)),
+                    transitionDuration: Duration.zero,
+                    reverseTransitionDuration: Duration.zero));
+            if (result != null) {
+              if ((result as bool) == true) {
+                _payees.remove(payee);
+                setState(() {
+                  _requireReconstruct = true;
+                });
+              }
+            }
+          }),
+      onTap: () async {
+        Object? result = await handlePaymentPage(payee);
+        if (result != null && (result as bool)) {
+          setState(() {
+            _requireReconstruct = true;
+          });
+        }
+      },
     );
+  }
+
+  bool _payeeSatisfyFilter(Payee payee) {
+    if (_filterKeyword.isEmpty) {
+      return true;
+    }
+
+    return payee.getNickName.toLowerCase().contains(_filterKeyword) ||
+        payee.getAccountName.toLowerCase().contains(_filterKeyword) ||
+        payee.accountID.bsb.contains(_filterKeyword) ||
+        payee.accountID.number.contains(_filterKeyword);
+  }
+
+  Future<Object?> handlePaymentPage(Payee payee) async {
+    Object? result = await Navigator.push(
+        context,
+        PageRouteBuilder(
+            pageBuilder: ((context, animation, secondaryAnimation) =>
+                PaymentPage(
+                  memberId: widget.memberId,
+                  accounts: widget.accounts,
+                  currAccount: widget.accounts[0],
+                  payee: payee,
+                ))));
+    return result;
   }
 }
