0c73007f46b0b63c25352d3b50a3404881a39435
Tao Hu <taohu22@qq.com>
2022-05-27 16:14:38 +0800

    Added to use EditingPageScaffold instead of scaffold

--- flutwest/lib/ui_page/payment_page.dart
+++ flutwest/lib/ui_page/payment_page.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutwest/controller/firestore_controller.dart';
 import 'package:flutwest/controller/sqlite_controller.dart';
 import 'package:flutwest/cust_widget/clickable_text.dart';
+import 'package:flutwest/cust_widget/cust_floating_button.dart';
 import 'package:flutwest/cust_widget/cust_text_field.dart';
+import 'package:flutwest/cust_widget/editing_page_scaffold.dart';
 import 'package:flutwest/cust_widget/in_text_button.dart';
 import 'package:flutwest/cust_widget/standard_padding.dart';
 import 'package:flutwest/model/account.dart';
@@ -33,7 +35,6 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-  final ScrollController _scrollController = ScrollController();
   final TextEditingController _tecMoney = TextEditingController();
   final TextEditingController _tecDescReceiver = TextEditingController();
   final TextEditingController _tecDescSender = TextEditingController();
@@ -42,12 +43,9 @@ class _PaymentPageState extends State<PaymentPage> {
   late Account _currAccount;
   bool _enabledPayButton = false;
   DateTime _payDateTime = DateTime.now();
-  String _appbarTitle = "";
 
   @override
   void initState() {
-    _scrollController.addListener(_onScroll);
-
     _currAccount = widget.currAccount;
 
     super.initState();
@@ -55,198 +53,160 @@ class _PaymentPageState extends State<PaymentPage> {
 
   @override
   void dispose() {
-    _scrollController.removeListener(_onScroll);
-
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: Text(_appbarTitle),
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: const Icon(Icons.close, color: Vars.clickAbleColor),
-        ),
-      ),
-      body: SingleChildScrollView(
-        controller: _scrollController,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: Vars.standardPaddingSize * 2),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: Vars.heightGapBetweenWidgets),
+    return EditingPageScaffold(
+        title: "Pay ${widget.payee.getNickName}",
+        content: [
+          const SizedBox(height: Vars.heightGapBetweenWidgets),
 
-              // The receiver
-              InTextButton.standard(
-                  leftLabel: "Pay ",
-                  label: widget.payee.getNickName,
+          // The receiver
+          InTextButton.standard(
+              leftLabel: "Pay ",
+              label: widget.payee.getNickName,
+              rightLabel: "",
+              onTap: () {
+                Navigator.pop(context);
+              }),
+          const SizedBox(height: Vars.gapBetweenTextVertical),
+          Text(
+              "${widget.payee.getAccountID.getBsb} ${widget.payee.accountID.getNumber}",
+              style: const TextStyle(
+                  fontSize: Vars.paragraphTextSize, color: Colors.black54)),
+          const SizedBox(height: Vars.heightGapBetweenWidgets),
+
+          // From which account
+          widget.accounts.length > 1
+              ? InTextButton.standard(
+                  leftLabel: "From ",
+                  label: _currAccount.getAccountName,
                   rightLabel: "",
-                  onTap: () {
-                    Navigator.pop(context);
-                  }),
-              const SizedBox(height: Vars.gapBetweenTextVertical),
-              Text(
-                  "${widget.payee.getAccountID.getBsb} ${widget.payee.accountID.getNumber}",
-                  style: const TextStyle(
-                      fontSize: Vars.paragraphTextSize, color: Colors.black54)),
-              const SizedBox(height: Vars.heightGapBetweenWidgets),
-
-              // From which account
-              widget.accounts.length > 1
-                  ? InTextButton.standard(
-                      leftLabel: "From ",
-                      label: _currAccount.getAccountName,
-                      rightLabel: "",
-                      onTap: () async {
-                        Object? result = await Navigator.push(
-                            context,
-                            PageRouteBuilder(
-                                pageBuilder:
-                                    ((context, animation, secondaryAnimation) =>
-                                        TransferFromPage(
-                                            accounts: widget.accounts,
-                                            requestResult: true)),
-                                transitionDuration: Duration.zero,
-                                reverseTransitionDuration: Duration.zero));
-                        if (result != null) {
-                          setState(() {
-                            _currAccount = result as Account;
-                          });
-                        }
-                      },
-                    )
-                  : InTextButton.noButton(
-                      leftLabel: "From ",
-                      rightLabel: _currAccount.getAccountName),
-              const SizedBox(height: Vars.gapBetweenTextVertical),
-              Text("\$${Utils.formatDecimalMoneyUS(_currAccount.getBalance)}",
-                  style: Vars.paragraphStyleGrey),
-              const SizedBox(height: Vars.heightGapBetweenWidgets * 1.5),
-
-              // Amount text field
-              const Text("Amount", style: Vars.headingStyle2),
-              CustTextField.moneyInput(
-                controller: _tecMoney,
-                onChanged: (String value) {
-                  Decimal? amount =
-                      Decimal.tryParse(value.replaceAll(r",", ""));
-
-                  if (amount == null) {
-                    if (_enabledPayButton) {
+                  onTap: () async {
+                    Object? result = await Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                            pageBuilder:
+                                ((context, animation, secondaryAnimation) =>
+                                    TransferFromPage(
+                                        accounts: widget.accounts,
+                                        requestResult: true)),
+                            transitionDuration: Duration.zero,
+                            reverseTransitionDuration: Duration.zero));
+                    if (result != null) {
                       setState(() {
-                        _enabledPayButton = false;
+                        _currAccount = result as Account;
                       });
                     }
-                  } else {
-                    if (!_enabledPayButton) {
-                      setState(() {
-                        _enabledPayButton = true;
-                      });
-                    }
-                  }
-                },
-                getErrorMsg: (String value) {
-                  Decimal? inputAmount =
-                      Decimal.tryParse(value.replaceAll(r",", ""));
+                  },
+                )
+              : InTextButton.noButton(
+                  leftLabel: "From ", rightLabel: _currAccount.getAccountName),
+          const SizedBox(height: Vars.gapBetweenTextVertical),
+          Text("\$${Utils.formatDecimalMoneyUS(_currAccount.getBalance)}",
+              style: Vars.paragraphStyleGrey),
+          const SizedBox(height: Vars.heightGapBetweenWidgets * 1.5),
 
-                  if (inputAmount != null) {
-                    if (inputAmount > _currAccount.balance) {
-                      return "Insufficient funds in your ${_currAccount.getAccountName} account.";
-                    }
-                  }
+          // Amount text field
+          const Text("Amount", style: Vars.headingStyle2),
+          CustTextField.moneyInput(
+            controller: _tecMoney,
+            onChanged: (String value) {
+              Decimal? amount = Decimal.tryParse(value.replaceAll(r",", ""));
 
-                  return null;
-                },
-              ),
-              const SizedBox(height: Vars.heightGapBetweenWidgets * 2.5),
+              if (amount == null) {
+                if (_enabledPayButton) {
+                  setState(() {
+                    _enabledPayButton = false;
+                  });
+                }
+              } else {
+                if (!_enabledPayButton) {
+                  setState(() {
+                    _enabledPayButton = true;
+                  });
+                }
+              }
+            },
+            getErrorMsg: (String value) {
+              Decimal? inputAmount =
+                  Decimal.tryParse(value.replaceAll(r",", ""));
 
-              // Schedule payment
-              InTextButton.standard(
-                leftLabel: "Schedule for ",
-                label: Utils.getDateTimeWDDMYToday(_payDateTime),
-                rightLabel: "",
-                onTap: () async {
-                  Object? result = await Navigator.push(
-                      context,
-                      PageRouteBuilder(
-                          pageBuilder:
-                              ((context, animation, secondaryAnimation) =>
-                                  SchedulePayPage(dateTime: _payDateTime)),
-                          transitionDuration: Duration.zero,
-                          reverseTransitionDuration: Duration.zero));
+              if (inputAmount != null) {
+                if (inputAmount > _currAccount.balance) {
+                  return "Insufficient funds in your ${_currAccount.getAccountName} account.";
+                }
+              }
 
-                  if (result != null) {
-                    DateTime newDateTime = result as DateTime;
-
-                    if (!Vars.isSameDay(newDateTime, _payDateTime)) {
-                      setState(() {
-                        _payDateTime = newDateTime;
-                      });
-                    }
-                  }
-                },
-              ),
-              const SizedBox(height: Vars.heightGapBetweenWidgets),
-
-              // Input description for receiver
-              CustTextField.standardSmall(
-                controller: _tecDescReceiver,
-                label: "Description for Test (optional)",
-                maxLength: 280,
-              ),
-              const SizedBox(height: Vars.heightGapBetweenWidgets),
-
-              // Input description for sender
-              CustTextField.standardSmall(
-                controller: _tecDescSender,
-                label: "Description for you (optional)",
-                maxLength: 35,
-              ),
-              const SizedBox(height: Vars.heightGapBetweenWidgets),
-
-              // Input reference
-              CustTextField.standardSmall(
-                controller: _tecReference,
-                label: "Description e.g. invoice number (optional)",
-                maxLength: 35,
-              ),
-              const SizedBox(height: Vars.heightGapBetweenWidgets * 2),
-
-              // Extra info
-              ClickableText.standard(text: "Things you should know"),
-
-              const SizedBox(height: 200)
-            ],
+              return null;
+            },
           ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Container(
-        padding: const EdgeInsets.all(Vars.standardPaddingSize),
-        color: Colors.white,
-        child: TextButton(
-          style: TextButton.styleFrom(
-              backgroundColor:
-                  _enabledPayButton ? Vars.clickAbleColor : Colors.grey,
-              splashFactory: NoSplash.splashFactory),
-          child: const Center(
-            heightFactor: 2.0,
-            child: Text(
-              "Pay",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
-            ),
+          const SizedBox(height: Vars.heightGapBetweenWidgets * 2.5),
+
+          // Schedule payment
+          InTextButton.standard(
+            leftLabel: "Schedule for ",
+            label: Utils.getDateTimeWDDMYToday(_payDateTime),
+            rightLabel: "",
+            onTap: () async {
+              Object? result = await Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                      pageBuilder: ((context, animation, secondaryAnimation) =>
+                          SchedulePayPage(dateTime: _payDateTime)),
+                      transitionDuration: Duration.zero,
+                      reverseTransitionDuration: Duration.zero));
+
+              if (result != null) {
+                DateTime newDateTime = result as DateTime;
+
+                if (!Vars.isSameDay(newDateTime, _payDateTime)) {
+                  setState(() {
+                    _payDateTime = newDateTime;
+                  });
+                }
+              }
+            },
           ),
-          onPressed: !_enabledPayButton
-              ? null
-              : () async {
+          const SizedBox(height: Vars.heightGapBetweenWidgets),
+
+          // Input description for receiver
+          CustTextField.standardSmall(
+            controller: _tecDescReceiver,
+            label: "Description for Test (optional)",
+            maxLength: 280,
+          ),
+          const SizedBox(height: Vars.heightGapBetweenWidgets),
+
+          // Input description for sender
+          CustTextField.standardSmall(
+            controller: _tecDescSender,
+            label: "Description for you (optional)",
+            maxLength: 35,
+          ),
+          const SizedBox(height: Vars.heightGapBetweenWidgets),
+
+          // Input reference
+          CustTextField.standardSmall(
+            controller: _tecReference,
+            label: "Description e.g. invoice number (optional)",
+            maxLength: 35,
+          ),
+          const SizedBox(height: Vars.heightGapBetweenWidgets * 2),
+
+          // Extra info
+          ClickableText.standard(text: "Things you should know"),
+
+          const SizedBox(height: 200)
+        ],
+        //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: !_enabledPayButton
+            ? CustFloatingButton.disabled(title: "Pay")
+            : CustFloatingButton.enabled(
+                title: "Pay",
+                onPressed: () async {
                   bool finishedPayment = false;
 
                   Decimal? amount =
@@ -309,10 +269,7 @@ class _PaymentPageState extends State<PaymentPage> {
                                             widget.memberId, widget.payee)))));
                     Navigator.pop(context, true);
                   }
-                },
-        ),
-      ),
-    );
+                }));
   }
 
   Future<void> handlePayment(
@@ -321,28 +278,12 @@ class _PaymentPageState extends State<PaymentPage> {
     await FirestoreController.instance.addPaymentTransaction(
         senderAccount: _currAccount,
         receiver: AccountID(number: "number", bsb: "bsb"),
-        receiverName: "test",
+        receiverName: widget.payee.getNickName,
         senderDescription: _tecDescSender.text,
         receiverDescription: _tecDescReceiver.text,
         amount: amount);
     await SQLiteController.instance
         .updatePayeeLastPayDate(memberId, payee.docId, payDate);
     payee.lastPayDate = payDate;
-  }
-
-  void _onScroll() {
-    if (_scrollController.offset > 10) {
-      if (_appbarTitle.isEmpty) {
-        setState(() {
-          _appbarTitle = "Pay";
-        });
-      }
-    } else {
-      if (_appbarTitle.isNotEmpty) {
-        setState(() {
-          _appbarTitle = "";
-        });
-      }
-    }
   }
 }
