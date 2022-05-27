a82db931f259cae7221a2b547d2bf1b933bddc4c
Tao Hu <taohu22@qq.com>
2022-05-27 16:12:55 +0800

    Change some styling

--- flutwest/lib/ui_page/transfer_from_page.dart
+++ flutwest/lib/ui_page/transfer_from_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutwest/cust_widget/cust_button.dart';
+import 'package:flutwest/cust_widget/cust_heading.dart';
 import 'package:flutwest/cust_widget/standard_padding.dart';
 import 'package:flutwest/model/account.dart';
+import 'package:flutwest/model/utils.dart';
 import 'package:flutwest/model/vars.dart';
 import 'package:flutwest/ui_page/transfer_page.dart';
 
@@ -38,14 +40,21 @@ class TransferFromPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: Vars.topBotPaddingSize),
-              StandardPadding(
+              //const SizedBox(height: Vars.topBotPaddingSize),
+              /*StandardPadding(
                 child: Text(
                   title,
                   style: Vars.headingStyle1,
                 ),
+              ),*/
+              CustHeading.big(
+                heading: title,
+                showHorPadding: true,
+                textStyle: const TextStyle(
+                    fontSize: Vars.headingTextSize1 + 4,
+                    fontWeight: FontWeight.w600),
               ),
-              const SizedBox(height: Vars.topBotPaddingSize * 2),
+              const SizedBox(height: Vars.heightGapBetweenWidgets),
               const StandardPadding(child: Text("Cash")),
               Column(
                 children: List.generate(accounts.length,
@@ -61,7 +70,7 @@ class TransferFromPage extends StatelessWidget {
   Widget _getAccountButton(Account account, BuildContext context) {
     return Padding(
       padding:
-          const EdgeInsets.symmetric(vertical: Vars.standardPaddingSize / 4),
+          const EdgeInsets.symmetric(vertical: Vars.standardPaddingSize / 3),
       child: CustButton(
         onTap: () {
           if (requestResult) {
@@ -79,18 +88,18 @@ class TransferFromPage extends StatelessWidget {
             }
           }
         },
-        headingStyle: const TextStyle(fontSize: Vars.headingTextSize3),
+        headingStyle: const TextStyle(fontSize: Vars.headingTextSize2),
         heading: account.getAccountName,
         paragraph: "${account.getBsb} ${account.getNumber}",
         rightWidget: Column(
           children: [
             Text(
-              account.getBalanceUSDToString,
-              style: Vars.headingStyle3,
+              "\$${Utils.formatDecimalMoneyUS(account.getBalance)}",
+              style: Vars.headingStyle2,
             ),
             const Text(
-              "Available",
-              style: TextStyle(fontSize: Vars.paragraphTextSizeSmall),
+              "available",
+              style: TextStyle(fontSize: Vars.paragraphTextSize),
             )
           ],
         ),
