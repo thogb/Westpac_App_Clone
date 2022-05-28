5ccc1ee4fcc8562e1f7e0cb8874edbbdfb9bdfa2
Tao Hu <taohu22@qq.com>
2022-05-28 12:44:10 +0800

    Updated cust appbar to include scroll controll toggle elevation and title on scroll

--- flutwest/lib/cust_widget/cust_appbar.dart
+++ flutwest/lib/cust_widget/cust_appbar.dart
@@ -4,7 +4,16 @@ class CustAppbar extends StatefulWidget implements PreferredSizeWidget {
   final Widget? leading;
   final Widget? title;
   final List<Widget>? trailing;
-  const CustAppbar({Key? key, this.leading, this.title, this.trailing})
+  final double showTitleOffset;
+  final ScrollController scrollController;
+
+  const CustAppbar(
+      {Key? key,
+      this.leading,
+      this.title,
+      this.trailing,
+      this.showTitleOffset = kToolbarHeight,
+      required this.scrollController})
       : preferredSize = const Size.fromHeight(kToolbarHeight),
         super(key: key);
 
@@ -15,11 +24,47 @@ class CustAppbar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _CustAppbarState extends State<CustAppbar> {
+  double _elevationLevel = 0;
+  Widget? _title;
+
+  @override
+  void initState() {
+    widget.scrollController.addListener(_onScroll);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.removeListener(_onScroll);
+
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (widget.scrollController.offset > widget.showTitleOffset) {
+      if (_title == null) {
+        setState(() {
+          _title = widget.title;
+          _elevationLevel = 3;
+        });
+      }
+    } else {
+      if (_title != null) {
+        setState(() {
+          _title = null;
+          _elevationLevel = 0;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      elevation: _elevationLevel,
       backgroundColor: Colors.grey[50],
-      title: widget.title,
+      title: _title,
       leading: widget.leading,
       actions: widget.trailing,
     );
