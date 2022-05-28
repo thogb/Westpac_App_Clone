f5f01cded5e05cfb12a2cbb2ffd97652f6bc8714
Tao Hu <taohu22@qq.com>
2022-05-28 15:11:58 +0800

    Added more parameters, in details page need work with multiple scroll controllers

--- flutwest/lib/cust_widget/cust_appbar.dart
+++ flutwest/lib/cust_widget/cust_appbar.dart
@@ -2,18 +2,22 @@ import 'package:flutter/material.dart';
 
 class CustAppbar extends StatefulWidget implements PreferredSizeWidget {
   final Widget? leading;
-  final Widget? title;
+  final String? title;
+  final String? subTitle;
   final List<Widget>? trailing;
   final double showTitleOffset;
   final ScrollController scrollController;
+  final List<ScrollController>? scrollControllers;
 
   const CustAppbar(
       {Key? key,
       this.leading,
       this.title,
+      this.subTitle,
       this.trailing,
       this.showTitleOffset = kToolbarHeight,
-      required this.scrollController})
+      required this.scrollController,
+      this.scrollControllers})
       : preferredSize = const Size.fromHeight(kToolbarHeight),
         super(key: key);
 
@@ -25,11 +29,18 @@ class CustAppbar extends StatefulWidget implements PreferredSizeWidget {
 
 class _CustAppbarState extends State<CustAppbar> {
   double _elevationLevel = 0;
-  Widget? _title;
+  bool _showTitle = false;
 
   @override
   void initState() {
     widget.scrollController.addListener(_onScroll);
+    if (widget.scrollControllers != null) {
+      for (ScrollController scrollController in widget.scrollControllers!) {
+        if (scrollController != widget.scrollController) {
+          scrollController.addListener(_onScroll);
+        }
+      }
+    }
 
     super.initState();
   }
@@ -37,22 +48,27 @@ class _CustAppbarState extends State<CustAppbar> {
   @override
   void dispose() {
     widget.scrollController.removeListener(_onScroll);
+    if (widget.scrollControllers != null) {
+      for (ScrollController scrollController in widget.scrollControllers!) {
+        scrollController.removeListener(_onScroll);
+      }
+    }
 
     super.dispose();
   }
 
   void _onScroll() {
     if (widget.scrollController.offset > widget.showTitleOffset) {
-      if (_title == null) {
+      if (_elevationLevel == 0) {
         setState(() {
-          _title = widget.title;
+          _showTitle = true;
           _elevationLevel = 3;
         });
       }
     } else {
-      if (_title != null) {
+      if (_elevationLevel != 0) {
         setState(() {
-          _title = null;
+          _showTitle = false;
           _elevationLevel = 0;
         });
       }
@@ -64,7 +80,27 @@ class _CustAppbarState extends State<CustAppbar> {
     return AppBar(
       elevation: _elevationLevel,
       backgroundColor: Colors.grey[50],
-      title: _title,
+      title: _showTitle
+          ? Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                widget.title != null
+                    ? Text(
+                        widget.title!,
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    : const SizedBox(),
+                widget.subTitle != null
+                    ? Text(widget.subTitle!,
+                        style: const TextStyle(
+                            color: Colors.black54, fontSize: 12.0))
+                    : const SizedBox()
+              ],
+            )
+          : null,
       leading: widget.leading,
       actions: widget.trailing,
     );
