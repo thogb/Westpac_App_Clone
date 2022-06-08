2ccaf68e97269237e62999187607ea8a5e90ae13
Tao Hu <taohu22@qq.com>
2022-06-08 19:58:13 +0800

    Added local auth, secured storage, changed splash loaing screen and added image assets and added few logos made with figma

--- flutwest/android/app/src/main/kotlin/com/example/flutwest/MainActivity.kt
+++ flutwest/android/app/src/main/kotlin/com/example/flutwest/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.example.flutwest
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+//class MainActivity: FlutterActivity() {
+//}
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
+    // ...
 }
--- flutwest/windows/flutter/generated_plugins.cmake
+++ flutwest/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,8 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  flutter_secure_storage_windows
+  local_auth_windows
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
